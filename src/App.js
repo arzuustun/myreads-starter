e21e65016ba8a0e3f59a6777d46f7441f1638e1e
austun10 <austun10@ford.com>
2020-10-19 23:44:39 +0300

    App.js created and other components added as child

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import React from "react";
+import "./App.css";
+import { getAll, update } from "./BooksAPI";
+import SearchBooks from "./components/SearchBooks";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Shelves from "./components/Shelves";
+
+class BooksApp extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      books: [],
+    };
+  }
+  componentDidMount() {
+    this.getBook();
+  }
+  getBook = async () => {
+    getAll().then((child) => {
+      console.log("getBook", child);
+      this.setState({ books: child });
+    });
+  };
+
+  updateBookShelf = (book, shelf) => {
+    const { books } = this.state;
+    book.shelf = shelf;
+
+    const findedBookIndex = books.findIndex(
+      (stateBook) => stateBook.id === book.id
+    );
+    if (findedBookIndex === -1) books.push(book);
+    else {
+      if (shelf === "none") {
+        books.splice(findedBookIndex, 1);
+      } else books[findedBookIndex] = book;
+    }
+
+    update(book, shelf).then(() => {
+      this.setState({ books });
+    });
+  };
+  render() {
+    return (
+      <Router>
+        <div className="app">
+          {console.log(this.state.books)}
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <Shelves
+                books={this.state.books}
+                onShelfChange={this.updateBookShelf}
+              />
+            )}
+          />
+          <Route
+            path="/search"
+            exact
+            render={() => (
+              <SearchBooks
+                books={this.state.books}
+                onShelfChange={this.updateBookShelf}
+              />
+            )}
+          />
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default BooksApp;
